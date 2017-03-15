9eccfeb8fae475314f800cd9dd5f0990558115a5
Colin Bloom <colinjbloom113@gmail.com>
2017-03-14 19:13:31 -0700

    add functionality to add equilateral triangles

--- App.java
+++ App.java
@@ -18,6 +18,29 @@ public class App {
         System.out.println("Great!! Please enter the length of side #1.");
         String stringSide1 = userConsole.readLine();
         Interger side1 = Integer.parseInt(stringSide1);
+        System.out.println("Thanks! What about side #2?");
+        String stringSide2 = userConsole.readLine();
+        Integer side2 = Interger.parseInt(stringSide2);
+        System.out.println("Fantastic. Finally, how long is side #3?");
+        String stringSide3 = userConsole.readLine();
+        Interger side3 = Interger.parseInt(stringSide3);
+
+        if ( side1 > side2 + side3 || side2 > side1 + side3 || side3 > side2 + side1 ) {
+          System.out.println("I'm sorry. Your inputted lengths do not form a trianlge. Please adjust the numbers.");
+        } else if ( side1 == side2 && side1 == side3 ) {
+          String type = "Equilateral";
+          Triangle newTriangle = new Triangle(side1, side2, side3, type);
+          allTriangles.add(newTriangle);
+          System.out.println("Great! Here is your triangle:");
+          System.out.println("--------------");
+          System.out.println( newTriangle.mSide1 );
+          System.out.println( newTriangle.mSide2 );
+          System.out.println( newTriangle.mSide3 );
+          System.out.println( newTriangle.mType );
+          System.out.println( "Your new triangle has been added to the queue." );
+        }
+
+        Triangle newTriangle = new Triangle(side1, side2, side3,);
 
       } else {
         System.out.println("Oh, darn. Well maybe next time!");
